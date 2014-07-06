8ba6d04ea5620d15f6831de8417a29ce06014c15
Sebastian Żurek <sebzur@codelab.pl>
2014-07-06 17:09:17 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from current_user.models import CurrentUserField
+
+class PublicationTracker(models.Model):
+    """ Stores the author, publication and update dates of entry. """
+
+    created_by = CurrentUserField(editable=False)
+    publication_datetime = models.DateTimeField(verbose_name="Publication datetime", auto_now_add=True, editable=False)
+    update_datetime = models.DateTimeField(verbose_name="Update datetime", auto_now=True, editable=False)
+    
+    class Meta:
+        abstract = True
