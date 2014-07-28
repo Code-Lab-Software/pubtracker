38d0923fd610b41670a6b642c91eb248cba111f3
CodeLAB <codelab@codelab.pl>
2014-07-28 22:20:48 +0200

    Related name to created_by added

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from current_user.models import CurrentUserField
 class PublicationTracker(models.Model):
     """ Stores the author, publication and update dates of entry. """
 
-    created_by = CurrentUserField(editable=False)
+    created_by = CurrentUserField(editable=False, related_name="%(app_label)s_%(class)s")
     publication_datetime = models.DateTimeField(verbose_name="Publication datetime", auto_now_add=True, editable=False)
     update_datetime = models.DateTimeField(verbose_name="Update datetime", auto_now=True, editable=False)
     
