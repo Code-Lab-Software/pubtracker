16cf6a4752e4d0df31f89e8e9096f21599e2c7d4
Sebastian Żurek <sebzur@codelab.pl>
2017-12-12 15:42:00 +0100

    Dodana metoda get_currnet_user

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from current_user.models import CurrentUserField
+from django.db.models.signals import pre_save
 
 class PublicationTracker(models.Model):
     """ Stores the author, publication and update dates of entry. """
@@ -10,3 +11,10 @@ class PublicationTracker(models.Model):
     
     class Meta:
         abstract = True
+
+    def get_current_user(self):
+        _user = self.created_by
+        pre_save.send(sender=self.__class__, instance=self, raw=True, using="default")
+        _current = self.created_by
+        self.created_by = _user
+        return _current
